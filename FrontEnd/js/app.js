1cfdc0eafcb5ade4898546828cdfd04cf41f8dfb
Boris Jekov <jekovboris@gmail.com>
2024-10-17 14:33:14 +0200

    methodappenChild

--- FrontEnd/js/app.js
+++ FrontEnd/js/app.js
@@ -1,22 +1,21 @@
-async function getWorks(filter) {
+async function getWorks(idCategory) {
   const url = "http://localhost:5678/api/works";
   try {
       const response = await fetch(url);
       if (!response.ok) {
           throw new Error(`Response status: ${response.status}`);
       }
-      const works = await response.json();
+      let works = await response.json();
       
-      // Appliquer le filtre si filter est défini
-      const filtered = filter !== undefined 
-          ? works.filter((data) => data.categoryId === filter) 
-          : works;
-
+      if (idCategory !== undefined){
+        works = works.filter((data) => data.categoryId === idCategory) 
+      } 
+      
       // Afficher les résultats filtrés
       document.querySelector(".gallery").innerHTML = ''; // Clear the gallery before appending
-      for (let i = 0; i < filtered.length; i++) {
-          console.log(filtered[i]);
-          setFigure(filtered[i]);
+      for (const work of works) {
+          console.log(work);
+          displayWork(work);
       }
   } catch (error) {
       console.error(error.message);
@@ -25,7 +24,7 @@ async function getWorks(filter) {
 
 getWorks(); // Appel initial sans filtre
 
-function setFigure(work) {
+function displayWork(work) {
   const figure = document.createElement("figure");
   figure.innerHTML = `<img src="${work.imageUrl}" alt="${work.title}">
                       <figcaption>${work.title}</figcaption>`;
@@ -46,9 +45,8 @@ async function getCategories() {
       document.querySelector(".tous").addEventListener("click", () => {
           getWorks(); // Appel sans filtre pour afficher tous les travaux
       });
-
-      for (let i = 0; i < categories.length; i++) {
-          setFilter(categories[i]);
+      for (const category of categories) {
+        setFilter(category);
       }
   } catch (error) {
       console.error(error.message);
@@ -59,9 +57,13 @@ getCategories();
 function setFilter(data) {
   const div = document.createElement("div");
   div.className = `${data.id}`;
-  div.innerHTML = `${data.name}`;
+
+  const textNode = document.createTextNode(data.name);
+  div.appendChild(textNode);
+  
   div.addEventListener("click", () => {
-      getWorks(data.id); // Appel de getWorks avec l'ID de la catégorie
+    getWorks(data.id); // Appel de getWorks avec l'ID de la catégorie
   });
-  document.querySelector(".div-container").append(div);
+  
+  document.querySelector(".div-container").appendChild(div);
 }
